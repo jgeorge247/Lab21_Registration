6f1371f488721ef57ca7ddf7eadf05c65a8a014a
jgeorge247 <justingeorge247@gmail.com>
2018-05-22 01:17:01 -0500

    first commit

---
+++ Lab21_Registration/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21_Registration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lab21_Registration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab21_Registration.MvcApplication" Language="C#" %>
